efceb096f434e16c8008f62d10643858129d2c24
正木啓介 <kettan0130@gmail.com>
2022-07-20 08:17:15 +0900

    値を保存できるようにした

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,22 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @board = current_user.board.build
+    @board = current_user.boards.build
   end
 
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path, notice:'投稿しました'
+    else
+      flash.now[:eoor] = '更新に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:introduction)
+  end
 end
