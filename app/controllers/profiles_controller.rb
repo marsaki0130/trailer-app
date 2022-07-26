3e4c7874a9aa2f3321483cdd87bc3891a0057b2f
正木啓介 <kettan0130@gmail.com>
2022-07-27 08:51:12 +0900

    user名を表示

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,7 +20,7 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save
-        redirect_to root_path, notice: 'プロフィールを作成できました'
+        redirect_to profile_path, notice: 'プロフィールを作成できました'
     else
         flash.now[:eoor] = '更新に失敗しました'
         render :new
